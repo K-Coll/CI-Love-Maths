45fd9b059f8a4315007584102ba448a15ddf8669
Katie Collins <katie93collins@gmail.com>
2024-05-13 21:33:43 +0000

    Add final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,8 @@ runGame("addition");
  */
 function runGame(gameType) {
 
+    document.getElementById("answer-box").value = "";
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
